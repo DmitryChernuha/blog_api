def98240f44fdb518ba3f1c86bd4a0efd494d28c
User <chernuhadmitry@gmail.com>
2023-12-10 21:29:27 +0300

    Add user registration

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from posts.views import PostDetail, PostList
+
 
 urlpatterns = [
     path('<int:pk>', PostDetail.as_view()),
-    path('', PostList.as_view())
+    path('', PostList.as_view()),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from posts.models import Post
+from posts.serializers import PostSerializer
+from posts.permission import IsAuthorOrReadOnly
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
